f3b3dd007dd497c25fd50015095d95af8e6428d0
aakash <aakash@2021.com.au>
2015-07-21 09:36:38 +1000

    26th commit

--- deploy.php
+++ deploy.php
@@ -30,10 +30,11 @@
 		$output .= htmlentities(trim($tmp)) . "\n";
 	}*/
 	
-    var_dump(shell_exec('sudo chown -R daemon:daemon ./2021test'.' 2>&1'));
+    /*var_dump(shell_exec('sudo chown -R daemon:daemon ./2021test'.' 2>&1'));
 	var_dump(shell_exec('git reset --hard HEAD'.' 2>&1'));
     var_dump(shell_exec('sudo git pull'.' 2>&1')); 
-    var_dump(shell_exec('git status')); 
+    var_dump(shell_exec('git status')); */
+	echo shell_exec('sh /opt/bitnami/apache2/htdocs/2021test/gitpull.sh');
 	// Make it pretty for manual user access (and why not?)
 ?>
 <!DOCTYPE HTML>
