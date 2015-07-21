11c86fbc6d3635110298a30b9f940f68a73e0677
aakash <aakash@2021.com.au>
2015-07-21 12:38:53 +1000

    40th commit

--- deploy.php
+++ deploy.php
@@ -30,11 +30,11 @@
 		$output .= htmlentities(trim($tmp)) . "\n";
 	}*/
 	
-    /*var_dump(shell_exec('sudo chown -R daemon:daemon ./2021test'.' 2>&1'));
-	var_dump(shell_exec('git reset --hard HEAD'.' 2>&1'));
-    var_dump(shell_exec('sudo git pull'.' 2>&1')); 
+    //var_dump(shell_exec('sudo chown -R daemon:daemon ./2021test'.' 2>&1'));
+	/*var_dump(shell_exec('git reset --hard HEAD'.' 2>&1'));
+    var_dump(shell_exec('git pull'.' 2>&1')); 
     var_dump(shell_exec('git status')); */
-	var_dump(shell_exec('sudo chown bitnami:bitnami /opt/bitnami/apache2/htdocs/2021test/gitpull.sh'.' 2>&1'));
+	/*var_dump(shell_exec('sudo chown bitnami:bitnami /opt/bitnami/apache2/htdocs/2021test/gitpull.sh'.' 2>&1')); */
 	echo shell_exec('sh /opt/bitnami/apache2/htdocs/2021test/gitpull.sh'.' 2>&1');
 	// Make it pretty for manual user access (and why not?)
 ?>
